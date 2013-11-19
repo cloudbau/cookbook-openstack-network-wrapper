ea0ec98e450a38867f542f497b04a65311e25c8b
m.benchchaoui@cloudbau.de <m.benchchaoui@cloudbau.de>
2013-11-19 10:45:38 +0100

    Detect which l2 agent to use, factory design pattern style.

---
+++ recipes/neutron-l2-agent.rb
@@ -0,0 +1,12 @@
+
+driver_name = node["openstack"]["network"]["interface_driver"].split('.').last.downcase
+driver_map = node["openstack"]["network"]["interface_driver_map"]
+main_plugin = driver_map[driver_name]
+
+if main_plugin == "openvswitch"
+  include_recipe "openstack-network::openvswitch"
+elsif main_plugin == "linuxbridge"
+  include_recipe "openstack-network::linuxbridge"
+else
+  raise NotImplementedError.new("Only support: #{driver_map.values.join(", ")}")
+end
