620b12a19bdcafdd7da70a099c0320fe8f3f572b
m.benchchaoui@cloudbau.de <m.benchchaoui@cloudbau.de>
2013-12-03 10:43:00 +0100

    Fix br-ex and external interface setup by using same logic as the stackforge cookbook and remove the stupid begin-rescue

--- recipes/neutron-l3-agent.rb
+++ recipes/neutron-l3-agent.rb
@@ -1,21 +1,23 @@
 include_recipe 'openstack-network::l3_agent'
 
-begin
+driver_name = node["openstack"]["network"]["interface_driver"].split('.').last.downcase
+main_plugin = node["openstack"]["network"]["interface_driver_map"][driver_name]
+
+if not ["nicira", "plumgrid", "bigswitch", "linuxbridge"].include?(main_plugin)
   ext_bridge = node["openstack"]["network"]["l3"]["external_network_bridge"]
   ext_bridge_iface = node["openstack"]["network"]["l3"]["external_network_bridge_interface"]
+
   cmd = "ovs-vsctl add-br #{ext_bridge}"
   # XXX(Mouad): If no external connection is required no need to add port
   # to iface, something that didn't occur to stackforge guys !!
-  if ext_bridge_iface == ""
+  if ext_bridge_iface.nil? or ext_bridge_iface.empty?
     cmd += " && ovs-vsctl add-port #{ext_bridge} #{ext_bridge_iface}"
   end
+
   rewind 'execute[create external network bridge]' do
     command cmd
     action :run
     not_if "ovs-vsctl show | grep 'Bridge #{ext_bridge}'"
     only_if "ip link show #{ext_bridge_iface}"
   end
-rescue Chef::Exceptions::ResourceNotFound
-  # No resource found mean that either we are in no openvwitch land, or that
-  # resource was renamed, hopefully not the latest :)
 end
