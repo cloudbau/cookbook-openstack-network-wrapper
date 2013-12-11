ec58d8c3c1317446b3b4e8725efeb004feefe4bf
m.benchchaoui@cloudbau.de <m.benchchaoui@cloudbau.de>
2013-12-11 11:15:02 +0100

    policy.conf file is not up to date with havana release in stackforge
    Do not use policy.conf from stackforge cookbook instead relay on our
    custom packages to offer a policy.conf file that is always up to date.

--- recipes/neutron-common.rb
+++ recipes/neutron-common.rb
@@ -1,10 +1,13 @@
 include_recipe 'openstack-network::common'
 
 
+# Custom rewind that take at the opposite of chef/rewind doesn't
+# assume that there is no duplicate resource, instead it will
+# "rewind" all resources with name `resource_id`.
 def cloudbau_rewind(resource_id, &block)
   run_context.resource_collection.each {
     |r| r.instance_exec(&block) if block and r.to_s == resource_id
-  }    
+  }
 end
 
 
@@ -23,4 +26,11 @@ platform_options["image_packages"].each do |pkg|
     options platform_options["package_overrides"]
     action :upgrade
   end
+end
+
+
+# This is hard to keep up to date and it's not needed because
+# our debian packages have the correct policy file already.
+rewind "template[/etc/quantum/policy.json]" do
+  action :nothing
 end
